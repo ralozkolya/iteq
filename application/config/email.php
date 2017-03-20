44272cb4d598cd4f7b2ea7ba1d833341cb3d53c3
Kolya <raloz1@gmail.com>
2017-03-20 14:37:45 +0400

    Add email config

---
+++ application/config/email.php
@@ -0,0 +1,6 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['email']['mailtype'] = 'html';
+$config['email']['newline'] = "\r\n";
+$config['email']['charset'] = 'utf-8';
