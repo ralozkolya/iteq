af63ff42aacce0ba109139eccb80f65cd0fb3957
Kolya <raloz1@gmail.com>
2017-04-07 18:06:04 +0400

    Add facebook page

--- application/views/pages/contact.php
+++ application/views/pages/contact.php
@@ -8,6 +8,14 @@
 	<script src="<?php echo static_url('js/contact.js?v='.V); ?>"></script>
 </head>
 <body>
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
 	<div class="wrapper">
 		<?php $this->load->view('elements/header'); ?>
 
@@ -25,6 +33,11 @@
 								</div>
 								<div class="col-sm-6">
 									<?php echo $page->body; ?>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-xs-12 text-center">
+									<div class="fb-page" data-href="https://www.facebook.com/Iteqgeorgia" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/Iteqgeorgia" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Iteqgeorgia">Iteq Georgia • აიტეკ ჯორჯია</a></blockquote></div>
 								</div>
 							</div>
 						</div>
