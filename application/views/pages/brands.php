74852cd5dcfd7e9da94ca1160d3345fb81b69d93
Kolya <raloz1@gmail.com>
2017-04-11 14:24:10 +0400

    Tweak brands page

--- application/views/pages/brands.php
+++ application/views/pages/brands.php
@@ -13,22 +13,22 @@
 			<div class="container">
 				<div class="row">
 					<?php if(!empty($brands)): ?>
-						<?php foreach($brands as $b): ?>
+						<?php $i = 0; foreach($brands as $b): ?>
+							<?php
+								$image = $b->image;
+
+								if(!$image) {
+									$image = static_url('img/no_image.png');
+								}
+
+								else {
+									$image = static_url('uploads/brands/thumbs/'.$image);
+								}
+							?>
 							<div class="col-sm-3">
 								<?php if($b->link): ?>
 									<a class="unstyled" href="<?php echo $b->link; ?>" target="_blank">
 										<div class="brand">
-											<?php
-												$image = $b->image;
-
-												if(!$image) {
-													$image = static_url('img/no_image.png');
-												}
-
-												else {
-													$image = static_url('uploads/brands/thumbs/'.$image);
-												}
-											?>
 											<div class="image"
 												style="background-image: url('<?php echo $image; ?>');"></div>
 											<div class="name"><?php echo $b->name; ?></div>
@@ -36,23 +36,15 @@
 									</a>
 								<?php else: ?>
 									<div class="brand">
-										<?php
-											$image = $b->image;
-
-											if(!$image) {
-												$image = static_url('img/no_image.png');
-											}
-
-											else {
-												$image = static_url('uploads/brands/thumbs/'.$image);
-											}
-										?>
 										<div class="image"
 											style="background-image: url('<?php echo $image; ?>');"></div>
 										<div class="name"><?php echo $b->name; ?></div>
 									</div>
 								<?php endif;?>
 							</div>
+							<?php $i++; if($i % 4 === 0) { ?>
+								<div class="clearfix"></div>
+							<?php } ?>
 						<?php endforeach; ?>
 					<?php else: ?>
 						<h3 class="text-center"><?php echo lang('no_brands'); ?></h3>
