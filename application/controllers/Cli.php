bff0d4af7014ad67a7851d72ec71bb42f0ad56d7
Kolya <raloz1@gmail.com>
2017-06-04 04:14:08 +0400

    Remove unnecessary white-space

--- application/controllers/Cli.php
+++ application/controllers/Cli.php
@@ -6,11 +6,8 @@ class Cli extends CI_Controller {
 	public function close_day() {
 
 		$this->load->library('Payment');
-
 		$this->payment->close_day();
-
 		$this->load->model('Order');
-
 		$this->Order->update_recents();
 
 		echo 'TBC Bank day last closed at: ' . date('Y-m-d H:i:s', strtotime('now')) . "\n";
